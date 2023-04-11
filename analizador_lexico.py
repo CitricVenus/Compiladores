eea46a92e3364f7315398de15b49fb155ab32fdf
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2023-04-10 20:37:27 -0600

    Update

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -27,18 +27,16 @@ tokens = {
     "=": "22",
     ";": "23",
     ",": "24",
-    '"': '25',
-    '.': '26',
-    "(": "27",
-    ")": "28",
-    "[": "29",
-    "]": "30",
-    "{": "31",
-    "}": "32",
-    "identifier": "33",
-    "string_value": "34",
-    "float_number": "35",
-    "int_number": "36"
+    "(": "25",
+    ")": "26",
+    "[": "27",
+    "]": "28",
+    "{": "29",
+    "}": "30",
+    "identifier": "31",
+    "string_value": "32",
+    "float_number": "33",
+    "int_number": "34"
 }
 # list con nuestras letras validas
 letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
@@ -51,36 +49,34 @@ delimitadores = ['+', '-', '*', '/', '>', '<', '!', '=', ';',
 # Nuestra tabla de transicione
 tabla_de_transicion = [
     [2, 3, 20, 21, 22, 6, 9, 10, 12, 11, 23, 24,
-        1, 31, 25, 26, 27, 28, 29, 30, 0, 41, 13],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-     14, 1, 1, 1, 1, 1, 1, 1, 1, 1, 40],
+        1, 38, 25, 26, 27, 28, 29, 30, 0, 38, 13,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 14, 1, 1, 1, 1, 1, 1, 1, 1, 1, 39],
     [2, 2, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
-     15, 15, 15, 15, 15, 15, 15, 15, 15, 41, 40],
+        38, 38, 15, 15, 15, 15, 15, 15, 15, 38, 39],
     [17, 3, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
-     17, 4, 17, 17, 17, 17, 17, 17, 17, 41, 40],
-    [42, 5, 42, 42, 42, 42, 42, 42, 42, 42, 42, 42,
-     42, 42, 42, 42, 42, 42, 42, 42, 42, 41, 40],
+        38, 4, 17, 17, 17, 17, 17, 17, 17, 38, 39],
+    [40, 5, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40,
+        40, 40, 40, 40, 40, 40, 40, 40, 40, 38, 39],
     [16, 5, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-     16, 16, 16, 16, 16, 16, 16, 16, 16, 41, 40],
+        38, 38, 16, 16, 16, 16, 16, 16, 16, 38, 39],
     [18, 18, 18, 18, 7, 18, 18, 18, 18, 18, 18, 18,
-     18, 18, 18, 18, 18, 18, 18, 18, 18, 41, 40],
-    [7, 7, 7, 7, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 40],
-    [7, 7, 7, 7, 7, 19, 7, 7, 7, 7, 7, 7,
-     7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 40],
+        38, 38, 18, 18, 18, 18, 18, 18, 18, 38, 39],
+    [7, 7, 7, 7, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 39],
+    [7, 7, 7, 7, 7, 19, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 39],
+    [31, 31, 31, 31, 31, 31, 31, 31, 31, 32, 31, 31,
+        38, 38, 31, 31, 31, 31, 31, 31, 31, 38, 39],
     [33, 33, 33, 33, 33, 33, 33, 33, 33, 34, 33, 33,
-     33, 33, 33, 33, 33, 33, 33, 33, 33, 41, 40],
+        38, 38, 33, 33, 33, 33, 33, 33, 33, 38, 39],
     [35, 35, 35, 35, 35, 35, 35, 35, 35, 36, 35, 35,
-     35, 35, 35, 35, 35, 35, 35, 35, 35, 41, 40],
-    [37, 37, 37, 37, 37, 37, 37, 37, 37, 38, 37, 37,
-     37, 37, 37, 37, 37, 37, 37, 37, 37, 41, 40],
-    [43, 43, 43, 43, 43, 43, 43, 43, 43, 39, 43, 43,
-     43, 43, 43, 43, 43, 43, 43, 43, 43, 41, 40]
+        38, 38, 35, 35, 35, 35, 35, 35, 35, 38, 39],
+    [41, 41, 41, 41, 41, 41, 41, 41, 41, 37, 41, 41,
+        41, 41, 41, 41, 41, 41, 41, 41, 41, 38, 39]
 ]
 # Los estados aceptores
 estados_aceptados = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-                     24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
+                     24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]
 # Estados de error
-estados_de_error = [40, 41, 42, 43]
+estados_de_error = [38, 39, 40, 41]
 # Diccionarios que nos ayudaran al manejo y almacenamiento de nuestros identificadores, strings, integers y floats
 tabla_identifieres = {}
 tabla_strings = {}
@@ -101,6 +97,7 @@ def analizador():
     cont_identifier = 1
     numero_aux = 0
     nuevo_estado = 0
+    imprime = True
 # El while termina asta que encuentre el EOF del archivo leido
     while 1:
         # Obtiene caracter por caracter del archivo que se está leyendo
@@ -124,8 +121,9 @@ def analizador():
                 if (avanza(estado_actual, char)):
                     # Verificamos que no sea fin del archivo
                     if (char == ''):
-                        # Si es fin del archivo, nos movemos al estado de error numero 40 EOF
-                        nuevo_estado = 40
+                        # Si es fin del archivo, nos movemos al estado de error numero 39 EOF
+                        nuevo_estado = 39
+
                     else:
                         # Se va contruyendo un string con los caracteres leidos
                         string_aux = string_aux + char
@@ -135,22 +133,22 @@ def analizador():
             # Se revisa si el estado actual es un estado de los compuestos >= ,<= , == , !=
             # Se almacena el numero del token en el output del escanner y se guarda en el reconocimiento de tokens
             if (estado_actual in estados_aceptados):
-                if (estado_actual == 36):
+                if (estado_actual == 33):
                     numero_aux = tokens.get(">=")
                     escaner_output.append(numero_aux)
                     reconocimiento_de_tokens.append(">=")
                     continue
-                if (estado_actual == 34):
+                if (estado_actual == 32):
                     numero_aux = tokens.get("<=")
                     escaner_output.append(numero_aux)
                     reconocimiento_de_tokens.append("<=")
                     continue
-                if (estado_actual == 38):
+                if (estado_actual == 36):
                     numero_aux = tokens.get("==")
                     escaner_output.append(numero_aux)
                     reconocimiento_de_tokens.append("==")
                     continue
-                if (estado_actual == 39):
+                if (estado_actual == 37):
                     numero_aux = tokens.get("!=")
                     escaner_output.append(numero_aux)
                     reconocimiento_de_tokens.append("!=")
@@ -170,28 +168,23 @@ def analizador():
                         if (not string_aux in tabla_strings):
                             # se usa para los strings, si char existe en la tabla de tokens, sigue
                             # Esto para poder sacar los tokens "
-                            if (char in tokens):
-                                # se saca el valor del token " para agregarlos al escaner output y al reconocimiento de tokens
-                                numero_aux = tokens.get(char)
-                                escaner_output.append(numero_aux)
-                                reconocimiento_de_tokens.append(char)
-                                # se agrega el string en un diccionario
-                                # se guarda el valor y el numero que le corresponde
-                                # cont_string guarda el valor numerico de la posicion en el diccionario
-                                tabla_strings[string_aux.replace(
-                                    '"', '')] = cont_string
-                                # se guarda el string en el reconocimiento de tokens
-                                reconocimiento_de_tokens.append(
-                                    string_aux.replace('"', ''))
-                                # se saca el numero del token String
-                                numero_aux = tokens.get("string_value")
-                                # Se crea un string que se guardara en el escaner output
-                                out_aux = str(numero_aux) + \
-                                    ','+str(cont_string)
-                                # se guarda el valor en el escaner output
-                                escaner_output.append(out_aux)
-                                # como ya se guardo un valor, nuestro contador se aumenta en uno para el siguiente que sea diferente al ya guardado
-                                cont_string += 1
+                            # se agrega el string en un diccionario
+                            # se guarda el valor y el numero que le corresponde
+                            # cont_string guarda el valor numerico de la posicion en el diccionario
+                            tabla_strings[string_aux.replace(
+                                '"', '')] = cont_string
+                            # se guarda el string en el reconocimiento de tokens
+                            reconocimiento_de_tokens.append(
+                                string_aux.replace('"', ''))
+                            # se saca el numero del token String
+                            numero_aux = tokens.get("string_value")
+                            # Se crea un string que se guardara en el escaner output
+                            out_aux = str(numero_aux) + \
+                                ','+str(cont_string)
+                            # se guarda el valor en el escaner output
+                            escaner_output.append(out_aux)
+                            # como ya se guardo un valor, nuestro contador se aumenta en uno para el siguiente que sea diferente al ya guardado
+                            cont_string += 1
                         # Si el string ya esta guardado, solo guardamos valores en el reconocimiento de tokens y en el output
                         # Se van a guardar con los valores del ya creado
                         else:
@@ -257,24 +250,27 @@ def analizador():
 # Si el estado actual es un estado de error , imprime cual fue el error
 # Si entra algun error, no se imprime las tablas
             if (estado_actual in estados_de_error):
-                if estado_actual == 40:
+                if estado_actual == 39:
                     print("EOF error")
-                    imprimeTablas(True)
+                    imprime = False
+                    # imprimeTablas(True)
+                    break
+                if estado_actual == 38:
+                    print(
+                        "Error, caracter no aceptado o hace falta delimitador " + "'" + char+"'")
+                    imprime = False
+                    # imprimeTablas(True)
+                    break
+                if estado_actual == 40:
+                    print("Error al crear valor flotante ")
+                    imprime = False
+                    # imprimeTablas(True)
                     break
                 if estado_actual == 41:
-                    print(
-                        "Error, caracter no aceptado o hace falta delimitador despues de " + '"' + string_aux + '"')
-                    imprimeTablas(True)
-                    break
-                if estado_actual == 42:
-                    print("Error al crear valor flotante" +
-                          string_aux + "No es un numero")
-                    imprimeTablas(True)
-                    break
-                if estado_actual == 43:
                     print("Error al crear el token" + ' "!=" ,' +
                           '"'+char + '"' + " es diferente a " + '"="')
-                    imprimeTablas(True)
+                    imprime = False
+                    # imprimeTablas(True)
                     break
             # Si char tiene el valor de un token, aqui revisamos que sea un token valido y lo agregamos al escaner output y al reconocimiento de tokens
             if (char in tokens):
@@ -285,11 +281,15 @@ def analizador():
     # Se cierra el archivo leido
     file.close()
     # Si no hubo errores, se imprime las tablas
-    imprimeTablas(False)
-
+    if imprime:
+        imprimeTablas(True)
+    else:
+        imprimeTablas(False)
 
 # Revisa si el siguiente estado es un estado de error, estado final o si es un estado no final. Esto dependiendo el caracter leido
 # Si no es un estado final, regresa True que significa que va a seguir leyendo mas caracteres
+
+
 def avanza(estado, char):
     siguiente_estado = 0
     if (char in letras):
@@ -485,7 +485,7 @@ def nuevoEstado(estado, char):
 
 # imprime las tablas
 def imprimeTablas(error_aux):
-    if (error_aux == False):
+    if (error_aux == True):
         print("---------------------------Reconocimiento de tokens----------------------\n")
         print(reconocimiento_de_tokens)
         print("---------------------------Output de escaner----------------------\n")
